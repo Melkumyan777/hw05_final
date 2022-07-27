4295b8b7c16190a20d75dbaeb33c403b93ef2221
Марат Мелкумян <melkumyanmarat9@gmail.com>
2022-07-28 00:11:48 +0300

    first try

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,21 +1,32 @@
 from http import HTTPStatus
+import tempfile
+import shutil
 
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.test import override_settings
 
 from posts.forms import PostForm
 from posts.models import Group, Post
 
 User = get_user_model()
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostsFormTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.form = PostForm()
         cls.user = User.objects.create_user(username='user')
+        cls.user_commentator = User.objects.create_user(
+            username='user_commentator'
+        )
         cls.group = Group.objects.create(
             title='Тестовая группа',
             slug='slug',
@@ -27,10 +38,57 @@ class PostsFormTest(TestCase):
             group=cls.group,
         )
 
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def setUp(self):
-        self.guest_client = Client()
+        self.guest_user = Client()
+        # Создаём авторизованный клиент
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
+        self.auth_user_for_comments = Client()
+        self.auth_user_for_comments.force_login(self.user_commentator)
+    
+    def test_authorized_user_create_post(self):
+        """Проверка создания записи авторизированным пользователем."""
+        posts_count = Post.objects.count()
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B')
+
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+        form_data = {
+            'text': 'Тестовый текст',
+            'group': self.group.id,
+            'image': uploaded,
+        }
+        response = self.authorized_user.post(
+            reverse('posts:create'),
+            data=form_data,
+            follow=True
+        )
+        self.assertRedirects(
+            response,
+            reverse(
+                'posts:profile',
+                kwargs={'username': self.user.username})
+        )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        post = Post.objects.get(id=self.post.id)
+        self.assertEqual(post.text, form_data['text'])
+        self.assertEqual(post.author, self.user)
+        self.assertEqual(post.group, form_data['group'])
+        self.assertEqual(post.image.name, 'posts/small.gif')
 
     def test_create_post_form(self):
         posts_count = Post.objects.count()
@@ -46,7 +104,7 @@ class PostsFormTest(TestCase):
         self.assertRedirects(response, reverse(
             'posts:profile', kwargs={'username': self.user}))
         self.assertEqual(Post.objects.count(), posts_count + 1)
-        post = Post.objects.latest('id')
+        post = Post.objects.get(id=self.post.id)
         self.assertEqual(post.text, form_data['text'])
         self.assertEqual(post.group, self.group)
         self.assertEqual(post.author, self.user)
